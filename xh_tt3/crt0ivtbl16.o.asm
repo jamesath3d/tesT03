0c0ae0810ea54c5549941fb190a9c2855b06810f
Your Name <you@example.com>
2021-10-20 16:44:24 -0400

    x

---
+++ xh_tt3/crt0ivtbl16.o.asm
@@ -0,0 +1,13 @@
+
+crt0ivtbl16.o:     file format elf32-msp430
+
+
+Disassembly of section .text:
+
+00000000 <__isr_0>:
+   0:	30 40 00 00 	br	#0x0000	
+
+Disassembly of section .vectors:
+
+00000000 <__ivtbl_16>:
+	...
